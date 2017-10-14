0176efd1647913354e936bb343ea7a60ba1a3a52
JeongHo16 <31890732+JeongHo16@users.noreply.github.com>
2017-10-14 19:07:41 +0900

    전화걸기 기능

--- app/src/main/java/com/hansung/teamproject/homework1/RestaurantDetailActivity.java
+++ app/src/main/java/com/hansung/teamproject/homework1/RestaurantDetailActivity.java
@@ -1,14 +1,18 @@
 package com.hansung.teamproject.homework1;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -19,6 +23,7 @@ public class RestaurantDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+    }
 
         /*ArrayList<MyItem> data = new ArrayList<MyItem>();
 
@@ -45,5 +50,11 @@ public class RestaurantDetailActivity extends AppCompatActivity {
             }
         });*/
 
-    }
+        public void call(View v){
+            TextView textView = (TextView)findViewById(R.id.phonenumber);
+            String callNum = textView.getText().toString();
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+callNum));
+            startActivity(intent);
+        }
+
 }
