974848aa00307c5a07f933fd7cd2519aea5a82df
Junhobear93 <31879021+Junhobear93@users.noreply.github.com>
2017-10-14 18:43:05 +0900

    item 넘겨 받기

--- app/src/main/java/com/hansung/teamproject/homework1/MenuDetailActivity.java
+++ app/src/main/java/com/hansung/teamproject/homework1/MenuDetailActivity.java
@@ -1,5 +1,6 @@
 package com.hansung.teamproject.homework1;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,6 +22,17 @@ public class MenuDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_detail);
+        Intent intent = getIntent();
+        int image = intent.getIntExtra("item_image", 1);
+        String name = intent.getStringExtra("item_name");
+        String price = intent.getStringExtra("item_price");
 
+        TextView textname = (TextView)findViewById(R.id.Item_name);
+        TextView textprice = (TextView)findViewById(R.id.Item_price);
+        ImageView textimage = (ImageView)findViewById(R.id.Item_image);
+
+        textname.setText(name);
+        textprice.setText(price);
+        textimage.setImageResource(image);
     }
 }
--- app/src/main/java/com/hansung/teamproject/homework1/RestaurantDetailActivity.java
+++ app/src/main/java/com/hansung/teamproject/homework1/RestaurantDetailActivity.java
@@ -1,9 +1,12 @@
 package com.hansung.teamproject.homework1;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,7 +18,7 @@ public class RestaurantDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
 
-        /*ArrayList<MyItem> data = new ArrayList<MyItem>();
+        ArrayList<MyItem> data = new ArrayList<MyItem>();
 
         data.add(new MyItem(R.drawable.noodle_soup, "손칼국수", "5.000"));
         data.add(new MyItem(R.drawable.bossam_formality, "보쌈 정식", "7.000"));
@@ -36,9 +39,16 @@ public class RestaurantDetailActivity extends AppCompatActivity {
                 int imageView = ((MyItem)adapter.getItem(i)).image;
                 String name = ((MyItem)adapter.getItem(i)).name;
                 String price = ((MyItem)adapter.getItem(i)).price;
-                Toast.makeText(RestaurantDetailActivity.this,  name + price, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MenuDetailActivity.class);
+                intent.putExtra("item_image", imageView);
+                intent.putExtra("item_name", name);
+                intent.putExtra("item_price", price);
+                startActivity(intent);
+
+
+
             }
-        });*/
+        });
 
     }
 }
