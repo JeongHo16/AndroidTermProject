bd3c995211f45545c6b3dda9217b895d4cf54fbb
Junhobear93 <31879021+Junhobear93@users.noreply.github.com>
2017-10-14 18:15:35 +0900

    check vision

--- app/src/main/java/com/hansung/teamproject/homework1/RestaurantDetailActivity.java
+++ app/src/main/java/com/hansung/teamproject/homework1/RestaurantDetailActivity.java
@@ -2,14 +2,9 @@ package com.hansung.teamproject.homework1;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
