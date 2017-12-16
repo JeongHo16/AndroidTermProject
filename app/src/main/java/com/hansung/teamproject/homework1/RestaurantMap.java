b6c99ebff6b063483b3b7a6a9b67937b894ab34f
JeongHo16 <31890732+JeongHo16@users.noreply.github.com>
2017-12-16 18:40:43 +0900

    지도 나타내기

--- app/src/main/java/com/hansung/teamproject/homework1/RestaurantMap.java
+++ app/src/main/java/com/hansung/teamproject/homework1/RestaurantMap.java
@@ -2,52 +2,100 @@ package com.hansung.teamproject.homework1;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class RestaurantMap extends AppCompatActivity implements OnMapReadyCallback{
+    final private int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 1;
 
-    final int REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA = 1;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mCurrentLocation ;
 
-    private FusedLocationProviderClient fusedLocationProviderClient;
     GoogleMap mGoogleMap = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_map);
 
-        checkDangerousPermissions(); // 위치 접근권한 확인
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (!checkLocationPermissions()) { //위치접근 권한 확인 11주차 강의자료 참고
+            requestLocationPermissions(REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION);
+        } else
+            getLastLocation();
     }
 
+    private boolean checkLocationPermissions() { //11주차 강의자료 참고
+        int permissionState = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermissions(int requestCode) { //11주차 강의자료 참고
+        ActivityCompat.requestPermissions(
+                RestaurantMap.this,            // RestaurantMap 액티비티의 객체 인스턴스를 나타냄
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},        // 요청할 권한 목록을 설정한 String 배열
+                requestCode    // 사용자 정의 int 상수. 권한 요청 결과를 받을 때
+        );
+    }
+
+    @Override //11주차 강의자료 참고
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastLocation();
+                } else {
+                    Toast.makeText(this, "Permission required", Toast.LENGTH_SHORT);
+                }
+                break;
+            }
+        }
+    }
+
+    @SuppressWarnings("MissingPermission") //11주차 강의자료 참고
+    private void getLastLocation() {
+        Task task = mFusedLocationClient.getLastLocation();       // Task<Location> 객체 반환
+        task.addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    mCurrentLocation = location;
+                    LatLng newLocation = new LatLng(mCurrentLocation.getLatitude(),
+                            mCurrentLocation.getLongitude());
+                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLocation,15));
+                } else
+                    Toast.makeText(getApplicationContext(),
+                            "No Location Detected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
-    }
-
-    private void checkDangerousPermissions(){
-        String[] permissions = {
-                Manifest.permission.ACCESS_FINE_LOCATION    // 위치 접근권한 확인
-        };
-        int permissionCheck = PackageManager.PERMISSION_GRANTED;
-        for(int i =0; i<permissions.length; i++){
-            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
-            if(permissionCheck == PackageManager.PERMISSION_DENIED)
-                break;
-        }
-        if(permissionCheck != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA);
     }
 }
